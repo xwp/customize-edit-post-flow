dc0342026279053c47f45b140189d5ea7658073e
Weston Ruter <weston@xwp.co>
2017-02-10 17:58:09 -0800

    Make clicking on edit post links more robust to support infnite scroll
    Incorporates code from https://github.com/xwp/wp-customize-posts/blob/f7b62df6/js/customize-preview-posts.js#L145-L170

--- js/customize-edit-post-flow.js
+++ js/customize-edit-post-flow.js
@@ -1,23 +1,27 @@
 (function( wp, $ ) {
 	var api = wp.customize;
 
-	function init() {
-		removeUnpreviewable();
-		followEditLinks();
-	}
+	// Prevent not-allowed cursor on edit-post-links.
+	api.isLinkPreviewable = ( function( originalIsLinkPreviewable ) {
+		return function( element, options ) {
+			if ( $( element ).closest( 'a' ).hasClass( 'post-edit-link' ) ) {
+				return true;
+			}
+			return originalIsLinkPreviewable.call( this, element, options );
+		};
+	} )( api.isLinkPreviewable );
 
-	function followEditLinks() {
-		$( 'body' ).on( 'click', '.post-edit-link', function( event ) {
-			api.preview.send( 'edit-post', event.target.href );
-			$( this ).css( 'cursor', 'progress' );
-		} );
-	}
+	// Send message to pane to request editing a post.
+	api.Preview.prototype.handleLinkClick = ( function( originalHandleLinkClick ) {
+		return function( event ) {
+			if ( $( event.target ).closest( 'a' ).hasClass( 'post-edit-link' ) ) {
+				event.preventDefault();
+				api.preview.send( 'edit-post', event.target.href );
+				$( event.target ).css( 'cursor', 'progress' );
+			} else {
+				originalHandleLinkClick.call( this, event );
+			}
+		};
+	} )( api.Preview.prototype.handleLinkClick );
 
-	function removeUnpreviewable() {
-		$( '.post-edit-link.customize-unpreviewable' ).removeClass( 'customize-unpreviewable' );
-	}
-
-	api.bind( 'preview-ready', function(){
-		setTimeout( init, 100);
-	} );
 })( this.wp, this.jQuery );
